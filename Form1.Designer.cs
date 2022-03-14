d8327085d467be6056bd7fb61a80eb0f30e8f356
JonaGrand <90683751+JonaGrand@users.noreply.github.com>
2022-03-14 19:47:23 +0100

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbConvers = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.txtEntrada = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txbConvers
+            // 
+            this.txbConvers.Location = new System.Drawing.Point(13, 13);
+            this.txbConvers.Multiline = true;
+            this.txbConvers.Name = "txbConvers";
+            this.txbConvers.Size = new System.Drawing.Size(424, 363);
+            this.txbConvers.TabIndex = 0;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(13, 419);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(203, 55);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(234, 419);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(203, 55);
+            this.btnConectar.TabIndex = 2;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // txtEntrada
+            // 
+            this.txtEntrada.Location = new System.Drawing.Point(13, 383);
+            this.txtEntrada.Name = "txtEntrada";
+            this.txtEntrada.Size = new System.Drawing.Size(424, 20);
+            this.txtEntrada.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 605);
+            this.Controls.Add(this.txtEntrada);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txbConvers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbConvers;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.TextBox txtEntrada;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using IBM.Cloud.SDK.Core.Authentication.Iam;
+using IBM.Cloud.SDK.Core.Http.Exceptions;
+using IBM.Watson.Assistant.v2;
+using IBM.Watson.Assistant.v2.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void conectar(string texto)
+        {
+            try
+            {
+                IamAuthenticator authenticator = new IamAuthenticator(apikey: "_DPGYFyIP4aUykVS53B4JFawtz261nvV4l9GGLUuN4eb");
+
+                AssistantService assistant = new AssistantService("2021-06-14", authenticator);
+                assistant.SetServiceUrl("https://api.eu-de.assistant.watson.cloud.ibm.com");
+
+                assistant.DisableSslVerification(true);
+
+                //Creem la sessio
+                var result = assistant.CreateSession(assistantId: "7ca45e38-0155-4856-8c0b-86e574c514b6");
+                //Escribimos la SessionID
+                Console.WriteLine(result.Response);
+                //Extraemos solo el SessionID de la respuesta
+                string sessionID = result.Response.Substring(20, 36);
+                //Escribimos el sessionID Limpio
+                Console.WriteLine("Session ID: " + sessionID);
+
+                //Mandamos un mensaje de prueba
+                var result2 = assistant.Message(
+                    assistantId: "7ca45e38-0155-4856-8c0b-86e574c514b6",
+                    sessionId: sessionID,
+                    input: new MessageInput()
+                    {
+                        Text = texto
+                    }
+                 );
+                    
+
+                Console.WriteLine(result2.Response);
+                txbConvers.Text = result2.Response;
+
+            }
+            catch (ServiceResponseException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            conectar(txtEntrada.Text);
+        }
+
+
+    }
+}
